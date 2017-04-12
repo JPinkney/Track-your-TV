e09bcfa1aab15ef94101d62c4f7e29b6d2b538e6
Josh Pinkney <Joshpinkney@gmail.com>
2017-04-12 02:07:43 -0400

    Started converting to local iamges

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,45 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "70%";
-    // document.getElementById("flipkart-navbar").style.width = "50%";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
+$(function(){
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.body.style.backgroundColor = "rgba(0,0,0,0)";
-}
+	function openNav() {
+	    document.getElementById("mySidenav").style.width = "70%";
+	    // document.getElementById("flipkart-navbar").style.width = "50%";
+	    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+	}
+
+	function closeNav() {
+	    document.getElementById("mySidenav").style.width = "0";
+	    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+	}
+
+	for(let x = 0; x < $('.show-container').length; x++){
+		$('#'+$('.show-container > .middle > .text')[x].id).on('click', function(){
+			$.post('/api/shows/removeFromUserViaID', {"showid": $('#'+$('.show-container > .middle > .text')[x].id)[0].id}, function(resp){
+				$('#'+$('.show-container > .middle > .text')[x].id).parent().parent().remove();
+			});
+		});
+	}
+
+	$('#search-button').on('click', function(){
+		//Do the request to the backend
+		var showToAdd = $('#search-input').val();
+
+		$.post('/api/shows/addShowToUser', {'showname': showToAdd}, function(result){
+
+			if(result.message){
+				alert(result.message);
+				$('#search-input').val('');
+			}else{
+				$('#middle-container').append('<div class="show-container"><img src=' + result.imagesrc + ' alt="Avatar" class="image"><div class="middle"><div id=' + result.id + ' class="text">Remove Show</div></div>');
+				$('#search-input').val('');
+
+				$('#'+result.id).on('click', function(){
+					$.post('/api/shows/removeFromUserViaID', {"showid": result.id}, function(resp){
+						$('#'+result.id).parent().parent().remove();
+					});
+				});
+			}
+
+		});
+	});
+
+});
